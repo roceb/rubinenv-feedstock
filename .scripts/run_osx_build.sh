954d82800d99fd57a34be375ad1ef59551b45740
Ross Ceballo <ross.ceballo@noirlab.edu>
2025-01-14 22:47:16 -0500

    Initial testing of rubin-env 10
    Update ci files to include py312 and numpy2
    
    Sync dev with main branch
    
    Remove numpy pin from recipe/meta.yaml
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.18.17.18.07
    
    Reverted Numpy changes
    
    Add Numpy migration
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.19.06.29.07
    
    Update cdc to match pinning feedstock
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.20.14.49.21
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.20.14.49.21
    
    Update c compiler and bump pin for python in pin-it output
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.21.03.00.28
    
    Remove pins for mac c_std_lib
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.21.03.00.28
    
    Update libcxx to match c compiler
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.21.10.12.57
    
    Set is_python_min  to false
    
    Co-authored-by: Matthew R. Becker <beckermr@users.noreply.github.com>
    Update recipe/meta.yaml
    fix: move to 312
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.21.10.12.57
    
    Added qp
    
    MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.21.20.56.13
    
    Update skyproj to >=2
    
    Update piff
    
    Add mermaid-py

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -40,10 +40,7 @@ conda activate base
 export CONDA_SOLVER="libmamba"
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
 
-mamba install --update-specs --quiet --yes --channel conda-forge --strict-channel-priority \
-    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1"
-mamba update --update-specs --yes --quiet --channel conda-forge --strict-channel-priority \
-    pip mamba conda-build conda-forge-ci-setup=4 "conda-build>=24.1"
+
 
 
 
@@ -108,8 +105,8 @@ else
 
     ( startgroup "Inspecting artifacts" ) 2> /dev/null
 
-    # inspect_artifacts was only added in conda-forge-ci-setup 4.6.0
-    command -v inspect_artifacts >/dev/null 2>&1 && inspect_artifacts || echo "inspect_artifacts needs conda-forge-ci-setup >=4.6.0"
+    # inspect_artifacts was only added in conda-forge-ci-setup 4.9.4
+    command -v inspect_artifacts >/dev/null 2>&1 && inspect_artifacts --recipe-dir ./recipe -m ./.ci_support/${CONFIG}.yaml || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
 
     ( endgroup "Inspecting artifacts" ) 2> /dev/null
     ( startgroup "Validating outputs" ) 2> /dev/null
